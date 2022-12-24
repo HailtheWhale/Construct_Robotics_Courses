6c290907c490ba94405cfa9f0af3da59b37d642e
HailtheWhale <dmg2000@att.net>
2022-12-24 01:20:38 +0000

    Add Linux Intro Course Stuff

---
+++ Introductory_Courses/Linux_Scripts/Advanced_Utilities_I/Exercise_3_4.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ARG1=$1
+			
+cd /home/user/catkin_ws/src/linux_course_files/move_bb8_pkg/src
+
+if [ "$ARG1" == 'circle' ]; then
+    echo "Moving in a CIRCLE!";
+	python move_bb8_circle.py
+elif [ "$ARG1" == 'forward_backward' ]; then
+    echo "Moving BACK and FORWARD!";
+	python move_bb8_circle.py
+elif [ "$ARG1" ==  'square'  ]; then
+    echo "Moving in a SQUARE!";
+	python move_bb8_square.py
+
+else
+echo "Please enter one of the following after calling this script:
+circle
+forward_backward
+square"
+
+fi
---
+++ Introductory_Courses/Linux_Scripts/Advanced_Utilities_II/test_process.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+
+class MoveBB8():
+
+    def __init__(self):
+        self.bb8_vel_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+        self.cmd = Twist()
+        self.ctrl_c = False
+        self.rate = rospy.Rate(1)
+        rospy.on_shutdown(self.shutdownhook)
+
+    def publish_once_in_cmd_vel(self):
+        """
+        This is because publishing in topics sometimes fails the first time you publish.
+        In continuous publishing systems, this is no big deal, but in systems that publish only
+        once, it IS very important.
+        """
+        while not self.ctrl_c:
+            connections = self.bb8_vel_publisher.get_num_connections()
+            if connections > 0:
+                self.bb8_vel_publisher.publish(self.cmd)
+                break
+            else:
+                self.rate.sleep()
+
+    def shutdownhook(self):
+        # works better than the rospy.is_shutdown()
+        self.stop_bb8()
+        self.ctrl_c = True
+
+    def stop_bb8(self):
+        rospy.loginfo("shutdown time! Stop the robot")
+        self.cmd.linear.x = 0.0
+        self.cmd.angular.z = 0.0
+        self.publish_once_in_cmd_vel()
+
+    def move_bb8(self, linear_speed=0.2):
+
+        self.cmd.linear.x = -linear_speed
+        self.cmd.angular.z = 0
+
+        while not self.ctrl_c:
+            self.publish_once_in_cmd_vel()
+            rospy.loginfo("Moving BB-8 forward!!")
+            self.rate.sleep()
+
+        self.stop_bb8()
+
+if __name__ == '__main__':
+    rospy.init_node('process_test', anonymous=True)
+    movebb8_object = MoveBB8()
+    try:
+        movebb8_object.move_bb8()
+    except rospy.ROSInterruptException:
+        pass
